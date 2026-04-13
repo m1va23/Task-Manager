5fd819780e39b865beebab8b22e9917fc8c9e8dd
Valera <m1vav21@gmail.com>
2026-04-13 16:18:54 +0300

    ver 0.0.3

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import json
 
-def create_task(name_tasks, *tasks):
+def create_task(name_tasks, task):
     user_task = {
-        name_tasks : [*tasks],
+        name_tasks : [task],
     }
-
     with open("tasks/test.json", 'w', encoding='utf-8') as file:
-        data = json.dump(user_task, file, indent=4)
-    return data
+        json.dump(user_task, file, indent=4)
+    print('Файл с задачами успешно создан')
 
-def append_task(name_tasks, *tasks):
-    # user_task = {
-    #     name_tasks : [*tasks]
-    # }
+
+def append_task(operation):
     with open('tasks/test.json', 'r', encoding='utf-8') as file:
         dicts = json.load(file)
 
-    dicts[name_tasks] = [*tasks]
+    if operation.lower() == 'файл':
+        try:
+            name_tasks = input('Имя файла: ')
+            dicts[name_tasks] = []
+            with open('tasks/test.json', 'w', encoding='utf-8') as file:
+                data = json.dump(dicts, file, indent=4)
+            print('Файл успешно создан')
+        except:
+            print('Произошла ошибка')
+        
+    elif operation.lower() == 'задача':
+        try:
+            task = [x for x in input('Введите свои задачи через пробел: ').split()]
+            name_file = input('Введите имя файла куда хотите сохранить задачи: ')
+            dicts[name_file] = task
 
-    with open('tasks/test.json', 'w', encoding='utf-8') as file:
-        data = json.dump(dicts, file, indent=4)
-    return data
+            with open('tasks/test.json', 'w', encoding='utf-8') as file:
+                data = json.dump(dicts, file, indent=4)
+            print('Задача успешна добавлена:)')
+        except:
+            print('Произошла ошибка')
 
 def read_task(name_tasks):
     '''Выводить все задачи, которые есть''' 
@@ -33,8 +46,46 @@ def clear_task():
     with open("tasks/test.json", 'w', encoding='utf-8') as file:
         pass
 
-create_task('tasks', 'приготовить', 'подрочить')
-append_task('task1', 'приготовить', 'подрочить')
-append_task('task2', 'подрочить')
-clear_task()
+def delete_task(name_tasks, task):
+    with open("tasks/test.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    data[name_tasks].remove(task)
 
+    with open('tasks/test.json', 'w', encoding='utf-8') as file:
+        data = json.dump(data, file, indent=4)
+
+def delete_task_vocab(name_tasks):
+    with open("tasks/test.json", 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    del data[name_tasks]
+
+    with open('tasks/test.json', 'w', encoding='utf-8') as file:
+        data = json.dump(data, file, indent=4)
+
+while True:
+    command = int(input('''
+1 - Создать первый файл с задачами
+2 - Создать задачу или файл
+3 - Просмотреть все задачи
+4 - Очистить задачи
+5 - Просмотреть выполненные задачи
+\n'''))
+    
+    match command:
+        case 1:
+            name_task_file = input('Введите файл для задач: ')
+            first_task = input('Напишите свою первую задачу: ')
+            create_task(name_task_file, first_task)
+
+            read_first_task = input(f"Желаете посмотреть задача в файле {name_task_file}?(Да/Нет):")
+            print('\nЗадачи:')
+            print(read_task(name_task_file)) if read_first_task == 'Да' else None
+
+        case 2:
+            operation = input('Что вы хотите создать(Файл/Задача): ')
+            append_task(operation)
+        case 3:
+            name_file = input('Введите имя файла для просмотра задач: ')
+            print(read_task(name_file))
